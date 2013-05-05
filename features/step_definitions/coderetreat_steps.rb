c767067c4ba84d35fb7e82ae3933ed4b83c67248
Maxim Filimonov <tpaktopsp@gmail.com>
2013-05-05 19:49:03 +1000

    Cucumber scenario with real db

--- features/step_definitions/coderetreat_steps.rb
+++ features/step_definitions/coderetreat_steps.rb
@@ -1,12 +1,12 @@
 Given(/^there are no coderetreats running today$/) do
+  Coderetreat.destroy_all
 end
 
-Coderetreat = Struct.new :status, :location
 Given(/^there are some coderetreats running today$/) do
   @coderetreats = [
-    Coderetreat.new('not_started', 'Chicago'),
-    Coderetreat.new('not_started', 'Seattle'),
-    Coderetreat.new('in_session', 'Sydney')
+    Coderetreat.create(status: 'not_started', location: 'Chicago'),
+    Coderetreat.create(status: 'not_started', location: 'Seattle'),
+    Coderetreat.create(status: 'in_session',  location: 'Sydney')
   ]
 end
 
