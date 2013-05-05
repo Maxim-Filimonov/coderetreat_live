e7a5564ebb4b1295c75c9964f3368ee13eac02a8
Maxim Filimonov <tpaktopsp@gmail.com>
2013-05-05 17:29:14 +1000

    Coderetreats collection presenter filter by status

---
+++ app/coderetreat_live/coderetreats/presenters/collection.rb
@@ -0,0 +1,24 @@
+module CoderetreatLive
+  module Coderetreats
+    module Presenters
+      class Collection
+        attr_reader :coderetreats
+        def initialize(coderetreats)
+          @coderetreats = coderetreats
+        end
+
+        def self.for(coderetreats)
+          new(coderetreats)
+        end
+        
+        def not_started(&block)
+          self.coderetreats.select{|cr| cr.status == 'not_started'}.each(&block)
+        end
+
+        def in_session(&block)
+          self.coderetreats.select{|cr| cr.status == 'in_session'}.each(&block)
+        end
+      end
+    end
+  end
+end
