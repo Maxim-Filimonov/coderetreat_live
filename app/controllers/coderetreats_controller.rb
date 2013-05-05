11a88cbd1c226a6a2eb1bd596b2b7e3554c6dbf0
Maxim Filimonov <tpaktopsp@gmail.com>
2013-05-05 16:02:20 +1000

    Generation of code retreats in controller

--- app/controllers/coderetreats_controller.rb
+++ app/controllers/coderetreats_controller.rb
@@ -1,5 +1,17 @@
 class CoderetreatsController < ApplicationController
+  Coderetreat = Struct.new :status, :location
   def running_today
-
+    @coderetreats = Object.new
+    def @coderetreats.not_started(&block)
+      [
+        Coderetreat.new('not_started', 'Chicago'),
+        Coderetreat.new('not_started', 'Seattle')
+      ].each(&block)
+    end
+    def @coderetreats.in_session(&block)
+      [
+        Coderetreat.new('in_session', 'Sydney')
+      ].each(&block)
+    end
   end
 end
