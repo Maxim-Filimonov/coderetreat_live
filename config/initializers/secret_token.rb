0179e935bfbf76b875509ddd19961ecac690ccf0
Maxim Filimonov <tpaktopsp@gmail.com>
2013-05-05 15:01:57 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CoderetreatLive::Application.config.secret_token = '16fd4ba09d5bbf8450abb6597aea58e6a222644e5781fa5949cd424b909d4c8d3c5f3c0a7351d683adca051c0e82d5870559e26fdfa16d16f281db78af9bbf21'
