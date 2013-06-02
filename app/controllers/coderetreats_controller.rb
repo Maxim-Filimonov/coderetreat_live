1c50d71f420bc9f5f139e7865332fe89cfc0f917
Maxim Filimonov <tpaktopsp@gmail.com>
2013-06-02 11:43:37 +1000

    Edit status view provides form for starting a session

--- app/controllers/coderetreats_controller.rb
+++ app/controllers/coderetreats_controller.rb
@@ -1,8 +1,11 @@
 require 'coderetreats'
 class CoderetreatsController < ApplicationController
-  Coderetreat = Struct.new :status, :location
   def running_today
 
     @coderetreats = CoderetreatLive::Coderetreats.running_today
+  end
+
+  def edit_status
+    @coderetreat = Coderetreat.find(params[:id])
   end
 end
