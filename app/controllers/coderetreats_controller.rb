69c8367785aac2282d424da1085b4a6c469a0bfd
Maxim Filimonov <tpaktopsp@gmail.com>
2013-06-02 12:00:06 +1000

    Coderetreats controller update status uses status updater

--- app/controllers/coderetreats_controller.rb
+++ app/controllers/coderetreats_controller.rb
@@ -1,11 +1,20 @@
 require 'coderetreats'
 class CoderetreatsController < ApplicationController
   def running_today
-
     @coderetreats = CoderetreatLive::Coderetreats.running_today
   end
 
   def edit_status
-    @coderetreat = Coderetreat.find(params[:id])
+    @coderetreat = Coderetreat.find(coderetreat_id)
+  end
+  
+  def update_status
+    CoderetreatLive::Coderetreats::Status.update_to(coderetreat_id, params[:new_status])
+    redirect_to edit_status_coderetreat_url(coderetreat_id)
+  end
+
+  private 
+  def coderetreat_id
+    coderetreat_id = params[:id]
   end
 end
