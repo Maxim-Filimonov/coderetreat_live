a63128f6d8be6469a006af76ad6a3f2a2a8f218f
Maxim Filimonov <tpaktopsp@gmail.com>
2013-06-02 12:07:13 +1000

    Status service updater

---
+++ spec/coderetreat_live/coderetreats/status_spec.rb
@@ -0,0 +1,12 @@
+require 'coderetreats/status'
+
+describe CoderetreatLive::Coderetreats::Status do
+  describe '.update_to' do
+    it "tells the coderetreat to update it's status" do
+      coderetreat = stub
+      stub_const('::Coderetreat', stub(find: coderetreat))
+      coderetreat.should_receive(:update_status).with('in_session')
+      CoderetreatLive::Coderetreats::Status.update_to('5', 'in_session')
+    end
+  end
+end
